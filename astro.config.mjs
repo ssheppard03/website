b47526f7cf83646e5dfd6f91f2a5851be992e68f
ssheppard03 <113851273+ssheppard03@users.noreply.github.com>
2023-07-03 15:41:53 -0230

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ssheppard03.github.io',
-  base: '/website',
   integrations: [tailwind(), react()]
 });
